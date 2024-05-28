64fd1339847c5c7b4da49989432bcb8ae738a5b0
Timothy Miamba <timal0361@gmail.com>
2024-05-28 20:45:13 +0200

    chore: Retrieve electronics data from fakeStoreApi

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,10 @@
+import { useData } from "../getData";
+
 const App = () => {
-  return;
+  const { data } = useData('https://fakestoreapi.com/products/category/electronics');
+  console.log(data);
+
+  return <h1>Hello World</h1>;
 };
 
 export default App;
