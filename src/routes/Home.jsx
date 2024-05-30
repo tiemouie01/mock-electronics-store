8c05a93ebec3ba38e12e8e72b885355ba1184412
Timothy Miamba <timal0361@gmail.com>
2024-05-30 21:10:37 +0200

    chore: Add background to Home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,18 @@
 const Home = () => {
-  return;
+  return (
+    <main className="home h-full">
+      <p className="text-white">
+        Photo by{" "}
+        <a href="https://unsplash.com/@cgower?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Christopher Gower
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/photos/black-cordless-headphones-beside-closed-black-laptop-computer-and-smartphone-_aXa21cf7rY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Unsplash
+        </a>
+      </p>
+    </main>
+  );
 };
 
 export default Home;
