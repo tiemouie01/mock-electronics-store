60bccce351b9d1a168e13a8c7c467ad1df723cd3
Timothy Miamba <timal0361@gmail.com>
2024-05-30 08:50:58 +0200

    chore: Add loading and error logic to API call

--- src/getData.js
+++ src/getData.js
@@ -1,16 +1,40 @@
 import { useState, useEffect } from "react";
 
-export const useData = (url) => {
-  const [state, setState] = useState();
+const fetchData = async (url) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error: Status ${response.status}`);
+  }
+
+  return response.json();
+};
+
+export const useData = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const dataFetch = async () => {
-      const data = await (await fetch(url)).json();
-      setState(data);
+      try {
+        const fetchedData = await fetchData(
+          "https://fakestoreapi.com/products/category/electronics"
+        );
+        setData(fetchedData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
     };
 
     dataFetch();
-  }, [url]);
+  }, []);
 
-  return { data: state };
+  return { data, error, loading };
 };
+
+// "https://fakestoreapi.com/products/category/electronics"
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,8 +1,17 @@
 import { useData } from "../getData";
+import { FaSpinner } from "react-icons/fa";
 
 const App = () => {
-  const { data } = useData('https://fakestoreapi.com/products/category/electronics');
+  const { data, error, loading } = useData();
+
   console.log(data);
+  console.log(error);
+  if (loading)
+    return (
+      <div className="flex justify-center items-center animate-spin h-dvh">
+        <FaSpinner size={50} />
+      </div>
+    );
 
   return <h1>Hello World</h1>;
 };
