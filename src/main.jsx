d4a685a7a35e5331df28f8e9fd6f6b7d1e1fd72c
Timothy Miamba <timal0361@gmail.com>
2024-05-30 20:37:25 +0200

    refactor: Update path to styles directory

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
-import "./index.css";
+import "./styles/index.css";
 
 const router = createBrowserRouter(routes);
 
