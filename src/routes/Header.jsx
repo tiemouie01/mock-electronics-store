76ee01b76895e0134933556e4ce66cf47defda43
Timothy Miamba <timal0361@gmail.com>
2024-05-30 20:24:24 +0200

    refactor: Update Header component to use a nav element for better semantics

--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -5,18 +5,20 @@ const Header = ({ cartItems }) => {
   return (
     <header>
       <h1>e-msika</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/shop">Shop</Link>
-        </li>
-        <li>
-          <FaCartShopping />
-          <span data-testId="cart-icon">{cartItems}</span>
-        </li>
-      </ul>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li>
+            <FaCartShopping />
+            <span data-testId="cart-icon">{cartItems}</span>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
