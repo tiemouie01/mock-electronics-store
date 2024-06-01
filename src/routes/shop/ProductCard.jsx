bd83e0bf6794cec44657eedfeb78f7b2a02ece83
Timothy Miamba <timal0361@gmail.com>
2024-06-01 13:17:59 +0200

    feat: Add page content and logic to Products section

--- src/routes/shop/ProductCard.jsx
+++ src/routes/shop/ProductCard.jsx
@@ -1,5 +1,15 @@
-const ProductCard = () => {
-  return;
+import { Link } from "react-router-dom";
+
+const ProductCard = ({ id, title, price, imageUrl, setSelectedProduct }) => {
+  return (
+    <article>
+      <Link to="/shop/detail" onClick={() => setSelectedProduct(id)}>
+        <img src={imageUrl} alt={`${title} photo`} className="w-[185px]"/>
+        <h3>{title}</h3>
+        <p>{price}</p>
+      </Link>
+    </article>
+  );
 };
 
 export default ProductCard;
--- src/routes/shop/Products.jsx
+++ src/routes/shop/Products.jsx
@@ -1,5 +1,22 @@
-const Products = () => {
-  return;
+import ProductCard from "./ProductCard";
+
+const Products = ({ products, setSelectedProduct }) => {
+  const cards = products.map((product) => (
+    <ProductCard
+      id={product.id}
+      title={product.title}
+      price={product.price}
+      imageUrl={product.image}
+      setSelectedProduct={setSelectedProduct}
+    />
+  ));
+
+  return (
+    <section>
+      <h2>Products</h2>
+      {cards}
+    </section>
+  );
 };
 
 export default Products;
