aec2ce98bf0fc31ac605ca0015fdf54a96c9fca8
Timothy Miamba <timal0361@gmail.com>
2024-06-01 15:08:13 +0200

    chore: Update Shop component to use context for cart items and selected product

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,12 +4,12 @@ import { Outlet } from "react-router-dom";
 
 const App = () => {
   const [cartItems, setCartItems] = useState([]);
-
+  
   return (
     <>
       <div className="h-[70px]"></div>
       <Header cartItems={cartItems.length} />
-      <Outlet />
+      <Outlet context={[cartItems, setCartItems]} />
     </>
   );
 };
--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import { useData } from "../../getData";
+import { FaSpinner } from "react-icons/fa6";
+import Products from "./Products";
+import { Outlet, useOutletContext } from "react-router-dom";
+
 const Shop = () => {
-  return;
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [cartItems, setCartItems] = useOutletContext();
+  const { data, error, loading } = useData();
+
+  const updateCartItems = (item) => {
+    const updatedItems = [...cartItems];
+    updatedItems.push(item);
+    setCartItems(updatedItems);
+  };
+
+  if (loading)
+    return (
+      <div className="flex h-dvh items-center justify-center">
+        <FaSpinner size={50} className="animate-spin" />;
+      </div>
+    );
+    
+  return (
+    <main>
+      <Products products={data} setSelectedProduct={setSelectedProduct} />
+      <Outlet context={[updateCartItems, selectedProduct, data, cartItems]} />
+    </main>
+  );
 };
 
 export default Shop;
