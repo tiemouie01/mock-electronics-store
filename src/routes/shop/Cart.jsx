d5c280279f6065eb7e337c89b40237a01dde45f4
Timothy Miamba <timal0361@gmail.com>
2024-06-01 15:39:23 +0200

    feat: Apply full functionality to Cart

--- src/routes/shop/Cart.jsx
+++ src/routes/shop/Cart.jsx
@@ -1,5 +1,48 @@
+import { useOutletContext } from "react-router-dom";
+import { FaTrash } from "react-icons/fa6";
+
 const Cart = () => {
-  return;
+  const context = useOutletContext();
+  const cartItems = context[3];
+  const setCartItems = context[4];
+
+  const clearCart = () => setCartItems([]);
+
+  const removeFromCart = (index) => {
+    const updatedCart = [...cartItems];
+    updatedCart.splice(index, 1);
+    setCartItems(updatedCart);
+  };
+
+  const products = cartItems.map((product, index) => {
+    return (
+      <tr>
+        <td>{product.title}</td>
+        <td>${product.price}</td>
+        <td>{product.quantity}</td>
+        <td>
+          <button onClick={() => removeFromCart(index)}>
+            <FaTrash />
+          </button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <section>
+      <h1>Products in Cart</h1>
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Quantity</th>
+        </thead>
+        {products}
+      </table>
+      <button onClick={() => clearCart()}>Checkout</button>
+    </section>
+  );
 };
 
 export default Cart;
